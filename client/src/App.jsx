cc69cd190d5fc0c31fa87c472025a1f870e0cfe3
siddharth <110058016+sidish15@users.noreply.github.com>
2024-03-04 19:46:16 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+
+import Profile from './pages/Profile';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+
+const App = () => {
+  return<BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/about' element={<About/>}/>
+  </Routes>
+  </BrowserRouter>
+    
+  }
+
+export default App
