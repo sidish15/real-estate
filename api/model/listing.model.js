b7aeb93c8f59480dcf0740568663ee012a6380d7
siddharth <110058016+sidish15@users.noreply.github.com>
2024-03-14 23:05:31 +0530

    updated changes

--- api/model/listing.model.js
+++ api/model/listing.model.js
@@ -5,31 +5,40 @@ const listingSchema=new mongoose.Schema({
                 required:true
         },
         description:{
-
+                type:String,
+                required:true
         },
         address:{
-
+                type:String,
+                required:true
         },
         regularPrice:{
-
+                type:Number,
+                required:true
         },
         discountPrice:{
-
+                type:Number,
+                required:true
         },
         bathrooms:{
-
+                type:Number,
+                required:true
         },
         bedrooms:{
-
+                type:Number,
+                required:true
         },
         furnished:{
-
+                type:Boolean,
+                required:true
         },
         parking:{
-
+                type:Boolean,
+                required:true
         },
         type:{
-
+                type:String,
+                required:true
         },
         offer:{
 
