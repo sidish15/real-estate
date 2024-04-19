af09253b4a0ff23b9a6d6e504e8b4f50fb7b1075
siddharth <110058016+sidish15@users.noreply.github.com>
2024-04-19 16:25:07 +0530

    complete about page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -218,36 +218,37 @@ const Profile = () => {
       >Show Listings
       </button>
       <p>{showListingError ? 'Error showing Listings' : ''}</p>
-      {userListings && userListings.length > 0 &&
+      {userListings && userListings.length > 0 && (
         <div className='flex flex-col gap-5'>
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
-          {userListings.map((listing) => <div key={listing._id} className='border rounded-lg p-3 flex
+          {userListings.map((listing) => (
+            <div key={listing._id} className='border rounded-lg p-3 flex
        justify-between items-center gap-4'>
 
-            <Link to={`/listing/${listing._id}`}>
-              <img src={listing.imageUrls[0]} alt="listing cover"
-                className='h-16 w-16 object-contain' />
-            </Link>
-            <Link className='text-slate-700 font-semibold flex-1
-        hover:underline truncate' to={`listing/${listing._id}`}>
-              <p>{listing.name}</p>
-            </Link>
-            <div className='flex flex-col items-center'>
-              <button onClick={() => handleListingDelete(listing._id)}
-                className='text-red-700'>
-                Delete
-              </button>
-              <Link to={`/update-listing/${listing._id}`}> 
-              {/* /update-listing/${listing._id}  already defined in app.jsx */}
-              <button className='text-green-700'>
-                Edit
-              </button>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt="listing cover"
+                  className='h-16 w-16 object-contain' />
               </Link>
-            </div>
+              <Link className='text-slate-700 font-semibold flex-1
+        hover:underline truncate' to={`listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col items-center'>
+                <button onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'>
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  {/* /update-listing/${listing._id}  already defined in app.jsx */}
+                  <button className='text-green-700'>
+                    Edit
+                  </button>
+                </Link>
+              </div>
 
-          </div>
-          )}
-        </div>
+            </div>
+          ))}
+        </div>)
 
       }
     </div>
