a6a96b3d017d233585101bf73b94e92f45efbc5a
siddharth <110058016+sidish15@users.noreply.github.com>
2024-05-01 17:39:13 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
+import path from 'path'
 dotenv.config();//initilizing dotenv
 
 
@@ -14,6 +15,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
         console.log(err);
 })
+
+const __dirname=path.resolve()
+
 const app = express();
 //we are not allowed to send any JSON to the server
 //we need to allow the JSON as the input 
@@ -22,11 +26,22 @@ app.use(express.json())
 app.use(cookieParser());//now u can get the inf from the cookie
 
 
+app.listen(3000, () => {
+        console.log(`Server is running on port 3000`)
+})
 
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter)
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 
 // middleware
 // err = the error which we will pass in next function
@@ -40,7 +55,3 @@ app.use((err, req, res, next) => {
         })
 })
 
-
-app.listen(3000, () => {
-        console.log(`Server is running on port 3000`)
-})
